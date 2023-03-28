3f8a5e12adb5e870d832b2c6c48648009c3c3524
Tessema <tessemamineo@yahoo.com>
2023-03-28 12:38:26 -0700

    task one

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-...
+'''
 Module of a Square class
-...
+'''
 
 
 class Square:
-    '''A simple calss that defines a square
+    '''A simple class that defines a square
     Attributes:
         size: The size of the square
     '''
 
     def __init__(self, size):
-        '''Intialaziaton method for the Square class
+        '''Initialization method for the Square class
         Args:
-            size: The size of the sqaure
+            size: The size of the square
         '''
         self.__size = size
